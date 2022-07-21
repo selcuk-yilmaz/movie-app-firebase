d63c886fd393c75c890691f0698c4cc891c7481e
selcuklu <yilmax4@gmail.com>
2022-07-21 12:46:39 +0300

    first commit

---
+++ src/auth/firebase.js
@@ -0,0 +1,78 @@
+import { initializeApp } from "firebase/app";
+import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
+
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://firebase.google.com/docs/web/learn-more#config-object
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+
+export const createUser=async(email,password,navigate,displayName)=>{
+    
+try {
+    let userCredential= await createUserWithEmailAndPassword(auth, email, password);
+    await updateProfile(auth.currentUser, {
+      displayName: displayName
+    });
+    navigate("/");
+    console.log(userCredential);
+} catch (error) {
+    console.log(error);
+}
+};
+
+//* https://console.firebase.google.com/
+//* => Authentication => sign-in-method => enable Email/password
+//! Email/password ile girişi enable yap
+export const signIn = async (email, password, navigate) => {
+  //? mevcut kullanıcının giriş yapması için kullanılan firebase metodu
+  try {
+    let userCredential = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    navigate('/');
+    console.log(userCredential);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const userObserver=(setCurrentUser)=>{
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+setCurrentUser(user)
+  } else {
+setCurrentUser(false)
+  }
+});
+};
+
+export const logOut =()=>{
+  signOut(auth);
+};
+
+
+export const signUpProvider=(navigate)=>{
+  const provider = new GoogleAuthProvider();
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      navigate("/")
+console.log(result);
+    })
+    .catch((error) => {
+console.log(error);
+    });
+}
