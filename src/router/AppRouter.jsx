0c2acb1a56176e2638d6f23265c983e77353e3e6
selcuklu <yilmax4@gmail.com>
2022-08-08 12:38:25 +0300

    added vdeo section

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,23 +1,37 @@
-import React from 'react';
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
-import Navbar from '../components/Navbar';
-import Login from '../pages/Login';
-import Main from '../pages/Main';
-import MovieDetail from '../pages/MovieDetail';
-import Register from '../pages/Register';
-
+import React from "react";
+import { useContext } from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Outlet,
+  Navigate,
+} from "react-router-dom";
+import Navbar from "../components/Navbar";
+import { AuthContext } from "../context/AuthContext";
+import Login from "../pages/Login";
+import Main from "../pages/Main";
+import MovieDetail from "../pages/MovieDetail";
+import Register from "../pages/Register";
 
 const AppRouter = () => {
+  const { currentUser } = useContext(AuthContext);
+  function PrivateRouter() {
+    return currentUser ? <Outlet /> : <Navigate to="/login" replace />;
+  }
+
   return (
-   <BrowserRouter>
-   <Navbar />
+    <BrowserRouter>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Main />}  />
-        <Route path="/login" element={<Login />}  />
-        <Route path="/register" element={<Register />}  />
-        <Route path="/details/:id" element={<MovieDetail />}  />
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/details/:id" element={<PrivateRouter />}>
+          <Route path="" element={<MovieDetail />} />
+        </Route>
       </Routes>
-   </BrowserRouter>
+    </BrowserRouter>
   );
 };
 
